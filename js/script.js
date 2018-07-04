7610f1e8967e35407e7c36f7f546adbf8929dcc0
Ivan <ifabrichnov@yandex.ru>
2018-07-04 10:51:48 +0300

    доделано

--- js/script.js
+++ js/script.js
@@ -1,44 +1,46 @@
-var search = document.querySelector('.search-button');
-var form = document.querySelector('.search-form');
-var arrival = form.querySelector('[name=date-arrival]');
-var exit = form.querySelector('[name=date-exit]');
-var adults = form.querySelector('[name=number-adults]');
-var children = form.querySelector('[name=number-children]');
-
-form.classList.remove('search-form-open');
+var link = document.querySelector(".search-button");
+var modal = document.querySelector(".modal-search");
+var arrival = modal.querySelector("[name=arrival]");
+var form = modal.querySelector("form");
+var departure = modal.querySelector("[name=departure]");
+var adult = modal.querySelector("[name=adult]");
+var child = modal.querySelector("[name=child]");
 
 var isStorageSupport = true;
-var storage_adults = '';
-var storage_children = '';
+var adultStorage = "";
+var childStorage = "";
 
-  try {
-    storage_adults = localStorage.getItem('adults');
-    storage_children = localStorage.getItem('children');
+
+document.documentElement .classList.replace('no-js', 'js');
+  
+try {
+    adultStorage = localStorage.getItem("adult");
+    childStorage = localStorage.getItem("child");
   } catch (err) {
     isStorageSupport = false;
   }
 
-search.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  form.classList.toggle('search-form-open');
-  form.classList.remove('search-form-error');
 
-  if (storage_adults || storage_children) {
-    adults.value = storage_adults;
-    children.value = storage_children;
-  }
-});
-
-form.addEventListener('submit', function(evt) {
-  if (!arrival.value || !exit.value || !adults.value || !children.value) {
+link.addEventListener("click", function (evt) {
     evt.preventDefault();
-    form.classList.remove('search-form-error');
-    form.offsetWidth = form.offsetWidth;
-    form.classList.add('search-form-error');
-  } else {
-    if (isStorageSupport) {
-      localStorage.setItem('adults', adults.value);
-      localStorage.setItem('children', children.value);
+    modal.classList.toggle("modal-show");
+    arrival.focus();
+    if (adultStorage && childStorage) {
+        adult.value = adultStorage;
+        child.value = childStorage;
     }
-  }
-});
+    modal.classList.remove("modal-error");
+  });
+
+form.addEventListener("submit", function (evt) {
+    if (!arrival.value || !departure.value || !adult.value || !child.value) {
+      evt.preventDefault();
+      modal.classList.remove("modal-error");
+      modal.offsetWidth = modal.offsetWidth;
+      modal.classList.add("modal-error");
+      console.log("Нужно заполнить все поля");
+    } else { if (isStorageSupport) {
+      localStorage.setItem("adult", adult.value);
+      localStorage.setItem("child", child.value); }
+    }
+  });
